5d874fe0efd9423104018d6b2792033b0bc71c14
刘金鑫 <adrianx.lau@gmail.com>
2021-02-23 21:52:58 +0800

    生克关系重构

--- element.cxx
+++ element.cxx
@@ -10,15 +10,6 @@ const char * ElementNames[] ={
     ELEMENT_TU
 };
 
-const ELEMENT_INDEX ElementRelation[] = {
-    ELEMENTINDEX_JIN,
-    ELEMENTINDEX_SHUI,
-    ELEMENTINDEX_MU,
-    ELEMENTINDEX_HUO,
-    ELEMENTINDEX_TU
-};
-
-
 const char * RelationNames[] = {
     RELATION_RESTRICT,
     RELATION_RESTRICTED,
@@ -27,6 +18,55 @@ const char * RelationNames[] = {
     RELATION_GENERATED,
     RELATION_INVALID
 };
+
+
+typedef struct {
+    ELEMENT_INDEX self;
+    ELEMENT_INDEX generate;
+    ELEMENT_INDEX restrict;
+    ELEMENT_INDEX restricted;
+    ELEMENT_INDEX generated;
+}RELATIONS;
+
+
+RELATIONS ElementRelation[]={
+    {
+        ELEMENTINDEX_JIN,
+        ELEMENTINDEX_SHUI,
+        ELEMENTINDEX_MU,
+        ELEMENTINDEX_HUO,
+        ELEMENTINDEX_TU
+    },
+    {
+        ELEMENTINDEX_SHUI,
+        ELEMENTINDEX_MU,
+        ELEMENTINDEX_HUO,
+        ELEMENTINDEX_TU,
+        ELEMENTINDEX_JIN
+    },
+    {
+        ELEMENTINDEX_MU,
+        ELEMENTINDEX_HUO,
+        ELEMENTINDEX_TU,
+        ELEMENTINDEX_JIN,
+        ELEMENTINDEX_SHUI
+    },
+    {
+        ELEMENTINDEX_HUO,
+        ELEMENTINDEX_TU,
+        ELEMENTINDEX_JIN,
+        ELEMENTINDEX_SHUI,
+        ELEMENTINDEX_MU
+    },
+    {
+        ELEMENTINDEX_TU,
+        ELEMENTINDEX_JIN,
+        ELEMENTINDEX_SHUI,
+        ELEMENTINDEX_MU,
+        ELEMENTINDEX_HUO
+    }
+};
+
 
 bool get_element_name(ELEMENT_INDEX element,char * name)
 {
@@ -40,46 +80,27 @@ bool get_element_name(ELEMENT_INDEX element,char * name)
 }
 ELEMENT_RELATION get_relation(ELEMENT_INDEX e1,ELEMENT_INDEX e2)
 {
-    for (size_t i = 0; i < sizeof(ElementRelation)/sizeof(ElementRelation[0]); i++)
-    {
-        if(ElementRelation[i] == e1)
-        {
-            for (size_t j = 0; j <  sizeof(ElementRelation)/sizeof(ElementRelation[0]); j++)
-            {
-                 if(ElementRelation[j] == e2)
-                 {
-                    if(i==j)
-                        return ER_SAME;
+    if(e1<0)
+        return ER_INVALID;
+    size_t count = sizeof(ElementRelation)/sizeof(ElementRelation[0]);
+    if(e1>count || e2> count)
+        return ER_INVALID;
+    
+    if(e1==e2)
+        return ER_SAME;
+    
+    if(ElementRelation[e1].generate == e2)
+        return ER_GENERATE;
+    
+    if(ElementRelation[e1].restrict == e2)
+        return ER_RESTRICT;
 
-                    //生
-                    if(i==j+1)
-                        return ER_GENERATE;
-                    if(i==j+4)//金 -> 土
-                        return ER_GENERATE;
+    if(ElementRelation[e1].generated == e2)
+        return ER_GENERATED;
 
-                    //被生                    
-                    if(i+1==j)
-                        return ER_GENERATED;
-                    if(i+4==j) //金 <- 土
-                        return ER_GENERATED;
-
-                    //克
-                    if(i==j+2)
-                        return ER_RESTRICT;
-                    if(i+3==j) // 土 -> 水
-                        return ER_RESTRICT;
-
-                    //被克
-                    if(i+2==j)
-                        return ER_RESTRICTED;
-                    if(i=j+3) // 土 <- 水
-                        return ER_RESTRICTED;
-
-                 }
-                 return ER_INVALID;
-            }
-        }
-    }
+    if(ElementRelation[e1].restricted == e2)
+        return ER_RESTRICTED;
+    
     return ER_INVALID;
 }
 
